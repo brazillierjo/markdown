938422f360b0eed61be075e274a0265c0c255a4c
brazillierjo <j.brazillier@gmail.com>
2021-01-16 18:36:32 +0100

    markdown push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import { sampleText } from './sampleText'
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText })
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = e => {
+    const text = e.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html: __html }
+  }
+
+  render() {
+    return (
+      <div className='container'>
+
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea onChange={this.handleChange} value={this.state.text} className='form-control' rows="35"></textarea>
+          </div>
+
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={ this.renderText(this.state.text) }></div>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
